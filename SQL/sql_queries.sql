5fe98d25823bb216a53d0885bb72b09e3b147945
Shweta S <shweta.shukla4@icloud.com>
2020-07-25 10:51:29 -0400

    ELT work

--- SQL/sql_queries.sql
+++ SQL/sql_queries.sql
@@ -1,5 +1,16 @@
+/*Queries  */
+SELECT *
+FROM olympicinfo
+
+/*Renaming column name  */
+SELECT olympicinfo.namee
+alter table  olympicinfo rename column namee to athlete;
+
+/*Dropping column  */
 select * from olympicinfo;
 alter table olympicinfo drop column noc;
+
+/*ELT Staging  */
 select * into table staging_olympic from olympicinfo where 1=1;
 select * from staging_olympic;
 SELECT * INTO TABLE final_olympicinfo FROM staging_olympic WHERE year between '1999' AND '2017';
